3d77a6fdafe41e3db60f83391132fb2874a34da4
Bionicsonic <bsonic2001@gmail.com>
2016-07-04 04:41:53 -0700

    TheStart
    Starting up some of the stuff

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+//[public] [Transform] = Choosable then [name of choice]
+	public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(target);
+	}
+}
